9106b1ef47a93a4e7db071eaced516c2d2136476
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-25 00:52:09 -0400

    feat: counting the number of students

--- csharp-exercise-typeset2/Program.cs
+++ csharp-exercise-typeset2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace csharp_exercise_typeset2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set = new HashSet<int>();
+            char l = 'A';
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"How many students for course {l}?");
+                int a = int.Parse(Console.ReadLine());
+                l++;
+                for (int j = 0; j < a; j++)
+                {
+                    set.Add(int.Parse(Console.ReadLine()));
+                }
+            }
+            Console.WriteLine("Total students: ");
+            Console.Write(set.Count);
+        }
+    }
+}
